a35091c2332be606c0578f50db498bb8c5dd0805
Yoann Menelet <yoann.menelet@gmail.com>
2015-07-16 18:37:11 +0200

    Much much changes
    GO

--- js/modules.js
+++ js/modules.js
@@ -9,6 +9,7 @@ var el = null;
 
 function router() {
     // Lazy load view element:
+    NProgress.start();
     el = el || document.getElementById('mainView');
 
 
@@ -21,11 +22,16 @@ function router() {
         // Render route template with John Resig's template engine:
         $("main").load("/partials/" + route.templateId + ".html", function(){
             componentHandler.upgradeDom();
+            NProgress.done();
         });
         $(".mdl-layout-title").text(route.templateId);
         
     }
 }
+
+setTimeout(function() {
+    $('body').css('opacity','1');
+},100);
 
 // Listen on hash change:
 window.addEventListener('hashchange', router);
@@ -36,8 +42,10 @@ route('/', 'Home');
 route('/Home', 'Home');
 route('/Me', 'Me');
 route('/Skills', 'Skills');
-route('/Recom', 'Recom');
+route('/Recommendation', 'Recommendation');
 route('/Diplomas', 'Diplomas');
 route('/Contact', 'Contact');
 route('/About', 'About');
 route('/Jobs', 'Jobs');
+
+NProgress.configure({parent:'#mainView'});
